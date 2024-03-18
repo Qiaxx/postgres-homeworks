fcf35cad3a9aeecae3cafde0e6a5b3e08609d6eb
Qiaxx <belozertsev04@mail.ru>
2024-03-18 18:31:01 +0300

    add docstrings

--- homework-1/data_loader.py
+++ homework-1/data_loader.py
@@ -7,7 +7,14 @@ import os
 class DataLoader:
     def __init__(self, conn_params, folder_name='north_data', customers_file='customers_data.csv',
                  employees_file='employees_data.csv', orders_file='orders_data.csv'):
-
+        """
+        Конструктор для файлов CSV и получения пути к файлам
+        :param conn_params: параметры подключения к БД
+        :param folder_name: название папки, где находятся файлы (по умолчанию название - 'north_data')
+        :param customers_file: название файла customers формата csv (по умолчанию название - 'customers_data.csv')
+        :param employees_file: название файла employees формата csv (по умолчанию название - 'employees_data.csv')
+        :param orders_file: название файла orders формата csv (по умолчанию название - 'orders_data.csv')
+        """
         self.conn_params = conn_params
         self.current_dir = os.path.dirname(__file__)
         self.csv_customers_path = os.path.join(self.current_dir, folder_name, customers_file)
@@ -15,6 +22,10 @@ class DataLoader:
         self.csv_orders_path = os.path.join(self.current_dir, folder_name, orders_file)
 
     def load_customers(self):
+        """
+        Метод для внесение данных в таблицу customers с подключением к БД
+        :return: нет
+        """
         with psycopg2.connect(**self.conn_params) as conn:
             with conn.cursor() as cursor:
                 with open(self.csv_customers_path, newline='') as csvfile:
@@ -25,6 +36,10 @@ class DataLoader:
                                        (row[0], row[1], row[2]))
 
     def load_employees(self):
+        """
+        Метод для внесение данных в таблицу employees с подключением к БД
+        :return: нет
+        """
         with psycopg2.connect(**self.conn_params) as conn:
             with conn.cursor() as cursor:
                 with open(self.csv_employees_path, newline='') as csvfile:
@@ -36,6 +51,10 @@ class DataLoader:
                                        (int(row[0]), row[1], row[2], row[3], birth_date, row[5]))
 
     def load_orders(self):
+        """
+        Метод для внесение данных в таблицу orders с подключением к БД
+        :return: нет
+        """
         with psycopg2.connect(**self.conn_params) as conn:
             with conn.cursor() as cursor:
                 with open(self.csv_orders_path, newline='') as csvfile:
